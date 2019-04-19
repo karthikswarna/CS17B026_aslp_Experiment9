da5a8e505dbebfc840823a51a86f69a1987a5f5a
karthikswarna <33567900+karthikswarna@users.noreply.github.com>
2019-04-19 12:29:08 +0530

    Update Q1.s

--- Q1.s
+++ Q1.s
@@ -1,11 +1,11 @@
 # MIPS program to use floating point instructions.
-			.data
+		.data
 float1:		.float	3.254464
 float2:		.float	5.654754
 double1:	.double	8.1456486787847986486
 double2:	.double	1.5648748677867
 	
-			.text
+		.text
 main:
 	l.s $f0, float1
 	l.s $f1, float2
